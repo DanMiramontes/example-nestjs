8bc84577d0a500187cecfc4802cb2e1d2a140ca3
DanMiramontes <dmiramontesg@gmail.com>
2023-07-15 20:33:25 -0600

    study guide nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from '../src/auth/auth.module';
+import { UserModule } from '../src/user/user.module';
+import { BookmarkModule } from '../src/bookmark/bookmark.module';
+import { PrismaModule } from '../src/prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
+})
+export class AppModule {}
---
+++ src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import {
+  ExecutionContext,
+  createParamDecorator,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request: Express.Request = ctx
+      .switchToHttp()
+      .getRequest();
+    if(data){
+        return request.user[data];
+    }
+    return request.user;
+  },
+);
